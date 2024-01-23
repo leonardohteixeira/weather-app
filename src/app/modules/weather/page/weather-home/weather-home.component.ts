ef553acddad54e46b3adc867c947ca60a0dcb492
Leonardo <leonardo.hteixeira@outlook.com.br>
2024-01-23 16:13:51 -0300

    Adicionado imagens na aplicação e consumido serviço de previsão do tempo

--- src/app/modules/weather/page/weather-home/weather-home.component.ts
+++ src/app/modules/weather/page/weather-home/weather-home.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../../services/weather.service';
+import { WeatherDatas } from 'src/app/models/WeatherDatas';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
   styleUrls: []
 })
-export class WeatherHomeComponent {
+export class WeatherHomeComponent implements OnInit {
+  initialCityName = 'Blumenau';
+  weatherDatas!: WeatherDatas;
 
+  constructor(private weatherService: WeatherService) {}
+  ngOnInit(): void {
+    this.getWeatherDatas(this.initialCityName);
+  }
+
+  getWeatherDatas(cityName: string): void{
+  this.weatherService.getWeatherDatas(cityName)
+  .subscribe({
+    next: (Response) => {
+      Response &&  (this.weatherDatas = Response);
+      console.log(this.weatherDatas)
+    },
+    error: (error) => console.log(error),
+  });
+  }
 }
