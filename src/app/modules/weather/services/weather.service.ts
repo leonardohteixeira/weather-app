ccfa2d338d0e9ab62b12356b6533195ec5aa6522
Leonardo <leonardo.hteixeira@outlook.com.br>
2024-01-23 15:49:47 -0300

    Gerado serviço de previsão do tempo

---
+++ src/app/modules/weather/services/weather.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WeatherService {
+  private apiKey = 'bbb2aa2d0b83e1cb52a57a88c9139a54';
+
+  constructor(private http: HttpClient) {}
+
+  getWeatherDatas(cityName: string): Observable<any> {
+    return this.http.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&mode=json&appid=${this.apiKey}`,
+      {}
+    );
+  }
+}
